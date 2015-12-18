00d45d36e597cb85ab11c7d8bfab52890168ce83
Hugo Briand <briand@ekino.com>
2015-12-18 15:18:47 +0100

    Issue 2.0.11 version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tc_aws',
-    version='2.0.10',
+    version='2.0.11',
     description='Thumbor AWS extensions',
     author='Thumbor-Community & William King',
     author_email='willtrking@gmail.com',
