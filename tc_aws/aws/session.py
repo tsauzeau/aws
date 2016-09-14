48a772c51c1c8c7e775472577ae6014db0b29f3c
Kevin Musker <kevinmusker@gmail.com>
2016-09-14 23:37:15 +0000

    Allow custom endpoint to be used. Workaround botocore's s3 hostname fixing

--- tc_aws/aws/session.py
+++ tc_aws/aws/session.py
@@ -1,11 +1,14 @@
 import botocore.session
+from botocore.utils import fix_s3_host
 
+__all__ = ['get_session']
 
-session = None
+sessions = [None, None]
 
-
-def get_session():
-    global session
+def get_session(endpoint=None):
+    session = sessions[bool(endpoint)]
     if session is None:
-        session = botocore.session.get_session()
+        session = sessions[bool(endpoint)] = botocore.session.get_session()
+        if endpoint:
+            session.unregister('before-sign.s3', fix_s3_host)
     return session
