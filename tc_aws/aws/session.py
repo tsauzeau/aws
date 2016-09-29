3f47e8b3ccb180d600dd23d12e1fc526814991ad
Your Name <you@example.com>
2016-09-29 22:56:48 +0000

    Further clarify session comment

--- tc_aws/aws/session.py
+++ tc_aws/aws/session.py
@@ -3,8 +3,8 @@ from botocore.utils import fix_s3_host
 
 __all__ = ['get_session']
 
-# We cache two sessions - one with a custom endpoint, one without)
-# So we have one session with the s3 signing hook for AWS connections
+# We cache two sessions
+# So we have one session with the s3 host-subdomain hook for AWS connections
 # and one without the hook for custom endpoints
 sessions = [None, None]
 
